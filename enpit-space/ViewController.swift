9ab963bf818c375b26fc3b379e7b4d8538757b3e
kengo Yokoyama <rbken.lo.ovl@gmail.com>
2015-07-17 16:47:31 +0900

    :heavy_plus_sign: 端末内の音楽を再生できるようにした

--- enpit-space/ViewController.swift
+++ enpit-space/ViewController.swift
@@ -7,19 +7,43 @@
 //
 
 import UIKit
+import MediaPlayer
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MPMediaPickerControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        configureMediaPicker()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    private func configureMediaPicker() {
+        let mediaPickerController = MPMediaPickerController()
+        mediaPickerController.delegate = self
+        mediaPickerController.allowsPickingMultipleItems = false
+        presentViewController(mediaPickerController, animated: true, completion: nil)
+    }
+    
+    func mediaPickerDidCancel(mediaPicker: MPMediaPickerController!) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func mediaPicker(mediaPicker: MPMediaPickerController!, didPickMediaItems mediaItemCollection: MPMediaItemCollection!) {
+        let item = mediaItemCollection.representativeItem
+        
+        let title = item.valueForProperty(MPMediaItemPropertyTitle) as! String
+        let artistName = item.valueForProperty(MPMediaItemPropertyArtist) as! String
+        let albumName = item.valueForProperty(MPMediaItemPropertyAlbumTitle) as! String
+        
+        let musicPlayer = MPMusicPlayerController()
+        musicPlayer.setQueueWithItemCollection(mediaItemCollection)
+        musicPlayer.play()
+    }
 }
 
